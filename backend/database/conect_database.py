615b73294d7eb4d8d5ac852ae359b709310e3178
Renata Marinho <renatareismarinho@gmail.com>
2026-05-04 14:38:36 -0300

    Update conect_database.py

--- backend/database/conect_database.py
+++ backend/database/conect_database.py
@@ -1,11 +1,26 @@
 # backend/database/db.py
-import sqlite3
-import os
 
-# Caminho para o banco de dados
-db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'database', 'Chamados.db')
+import os
+import psycopg2
+import psycopg2.extras
 
 def get_db_connection():
+    database_url = os.getenv("postgresql://postgres:UXVOHUVgbhbTNwvUYxMGttuZQZSOiwBG@postgres.railway.internal:5432/railway")
+
+    # Se existir DATABASE_URL → usa PostgreSQL (Railway)
+    if database_url:
+        conn = psycopg2.connect(
+            database_url,
+            sslmode="require"
+        )
+        return conn
+
+    # Senão → fallback para SQLite (local)
+    import sqlite3
+    db_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        '..', '..', 'database', 'Chamados.db'
+    )
     conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row  # Para retornar resultados como dicionários
+    conn.row_factory = sqlite3.Row
     return conn
