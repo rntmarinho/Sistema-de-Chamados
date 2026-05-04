59dffccb17735eee30c169f122826095c248213e
Renata Marinho <renatareismarinho@gmail.com>
2026-05-04 15:44:20 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,12 +16,12 @@ app.register_blueprint(user_controller, url_prefix='/api')
 load_dotenv()
 
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    
     email_thread = threading.Thread(target=iniciar_daemon_email, daemon=True)
     email_thread.start()
     
     print("-> Serviço de integração IMAP iniciado em background.")
     
     # 2. Inicia o servidor Flask[cite: 1]
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
