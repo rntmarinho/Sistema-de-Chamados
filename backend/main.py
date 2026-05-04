ec85124607955a8e2b63be83cc660e18097b3e78
Renata Marinho <renatareismarinho@gmail.com>
2026-05-04 15:36:23 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from users.user_controller import user_controller
 # Importa a função de background do novo serviço[cite: 1]
 from services.email_service import iniciar_daemon_email
 from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
@@ -15,8 +16,8 @@ app.register_blueprint(user_controller, url_prefix='/api')
 load_dotenv()
 
 if __name__ == '__main__':
-    # 1. Inicia o monitoramento de e-mails em uma thread separada[cite: 1]
-    # Isso evita que o loop de 5 minutos trave a sua API Flask[cite: 1]
+   port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     email_thread = threading.Thread(target=iniciar_daemon_email, daemon=True)
     email_thread.start()
     
