38b3626ea4a5bb5450fd27054eafcc2e00407001
Renata <renatareismarinho@gmail.com>
2026-05-11 14:59:14 -0300

    cadastro de categoria

--- backend/categories/category_controller.py
+++ backend/categories/category_controller.py
@@ -1,20 +1,20 @@
-# backend/categories/category_controller.py
 from flask import Blueprint, request, jsonify
-from categories.category_model import get_all_categories, create_category, delete_category
+from database.conect_database import get_db_connection
 
 category_controller = Blueprint('category_controller', __name__)
 
 @category_controller.route('/categories', methods=['GET'])
-def list_categories():
-    return jsonify(get_all_categories()), 200
+def get_categories():
+    conn = get_db_connection()
+    categories = conn.execute("SELECT * FROM tbl_categories").fetchall()
+    conn.close()
+    return jsonify([dict(cat) for cat in categories])
 
 @category_controller.route('/categories', methods=['POST'])
 def add_category():
     data = request.get_json()
-    create_category(data['nome'])
-    return jsonify({"message": "Categoria criada!"}), 201
-
-@category_controller.route('/categories/<int:id>', methods=['DELETE'])
-def remove_category(id):
-    delete_category(id)
-    return jsonify({"message": "Categoria removida!"}), 200
+    conn = get_db_connection()
+    conn.execute("INSERT INTO tbl_categories (nome) VALUES (?)", (data['nome'],))
+    conn.commit()
+    conn.close()
+    return jsonify({"message": "Sucesso"}), 201
